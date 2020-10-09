50e58e59db324b8909cb18d7596639888f0d00e3
syaifulfahmi <52196122+syaifulfahmi@users.noreply.github.com>
2020-10-09 14:28:35 +0700

    Add files via upload

---
+++ tugas_for_2.php
@@ -0,0 +1,19 @@
+<?php
+  for($a=12; $a>0; $a--){
+    for($i=1; $i<=$a; $i++){
+      echo "_";
+    }for($a1=12; $a1>$a; $a1--){
+      echo"x";
+    }for($a2=12; $a2>$a; $a2--){
+      echo"x";
+    }echo"<br>";
+    }for($b=0; $b<=12; $b++){
+    for($j=1; $j<=$b; $j++){
+      echo"_";
+    }for($b1=12; $b1>$b; $b1--){
+      echo"x";
+    }for($b2=12; $b2>$b; $b2--){
+      echo"x";
+    }echo"<br>";
+  }
+?>
